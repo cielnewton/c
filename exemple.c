6904c72d86f4fe8710db17558dd7616d0a13d895
cielnewton <76662720+cielnewton@users.noreply.github.com>
2025-01-27 12:47:57 +0100

    Create exemple.c

---
+++ exemple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int valeur = 0;
+
+    printf("Hello world!\n");
+    printf("Sasir un entier positif : ");
+    scanf("%d",&valeur);
+    printf("Vous avez saisi: %d",valeur);
+
+    return 0;
+}
